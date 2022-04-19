0be26b900db23c04de87e218dddfe84ab3d8c152
kwj1270 <kwj1270@naver.com>
2022-04-19 21:17:03 +0900

    feat(kwj1270): 프로젝트 로깅 로직 완성

---
+++ application/logv2/src/main/java/com/poc/logv2/Logv2Application.java
@@ -0,0 +1,13 @@
+package com.poc.logv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Logv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Logv2Application.class, args);
+    }
+
+}
